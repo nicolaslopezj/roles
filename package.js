f708c450ba1922af5cb3f2fd9523573da2eac84d
Nicolas Lopez <nicolaslopezj@me.com>
2018-06-14 15:56:45 -0400

    publish version 2.6.6

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   name: 'nicolaslopezj:roles',
   summary: 'The most advanced roles package for meteor',
-  version: '2.6.5',
+  version: '2.6.6',
   git: 'https://github.com/nicolaslopezj/roles'
 })
 
-Package.onUse(function (api) {
+Package.onUse(function(api) {
   api.versionsFrom('1.0')
 
   api.use([
@@ -18,29 +18,19 @@ Package.onUse(function (api) {
     'underscore'
   ])
 
-  api.use([
-    'templating'
-  ], {weak: true})
+  api.use(['templating'], {weak: true})
 
-  api.addFiles([
-    'helpers.js',
-    'roles.js',
-    'keys.js'
-  ])
+  api.addFiles(['helpers.js', 'roles.js', 'keys.js'])
 
-  api.addFiles([
-    'roles_server.js'
-  ], 'server')
+  api.addFiles(['roles_server.js'], 'server')
 
-  api.addFiles([
-    'roles_client.js'
-  ], 'client')
+  api.addFiles(['roles_client.js'], 'client')
 
   api.export('Roles')
   api.export('objectHasKey')
 })
 
-Package.onTest(function (api) {
+Package.onTest(function(api) {
   api.use('tinytest')
   api.use('orionjs:core')
 })
